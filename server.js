38122d8e5c3ca058307af4b2ede613462bfa9137
Goose <josephtrop@gmail.com>
2017-08-01 23:47:53 -0700

    TEST

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller");
-app.use("/", routes);
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+db.sequelize.sync({force: true}).then(()=>{
+    app.listen(port);
+})
+
+
+//app.use("/", routes);
 app.listen(port);
