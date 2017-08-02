c55229f87a671dc92d1277bbbaa4c0bc0c4b9424
Goose <josephtrop@gmail.com>
2017-08-02 01:08:13 -0700

    TEST

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -17,6 +17,19 @@ module.exports =  function(app){
         }).then(()=>{
             res.redirect("/")
         })
+    }),
+
+    app.put("/api/:id", (req,res)=>{
+        let col = Object.keys(req.body)[0]
+        let val = req.body[col]
+        db.burger.update(
+            {devoured: val},
+            {id: req.params.id}
+        ).success(()=>{
+            console.log("update success")
+        }).error((err)=>{
+            console.log("update err ", err)
+        })
     })
 
 
